7cb0654a5405229a42ef47c0d67596bb78a4ab28
Alexandr <ragozzzzinalexandr@gmail.com>
2022-12-03 16:01:18 +0500

    added example2

--- Example2/Program.cs
+++ Example2/Program.cs
@@ -1,17 +1,34 @@
-﻿int[] array = { 175, 24, 34, 466, 34, 63, 34, 91 };
+﻿// int[] array = { 175, 24, 34, 466, 34, 63, 34, 91 };
 
-int n = array.Length;
-int find = 34;
+// int n = array.Length;
+// int find = 34;
 
-int index = 0;
+// int index = 0;
 
-while (index < n)
-{
-    if(array[index] == find)
-    {
-        Console.WriteLine(index);
-        break;
-    }
-    index++;
+// while (index < n)
+// {
+//     if(array[index] == find)
+//     {
+//         Console.WriteLine(index);
+//         break;
+//     }
+//     index++;
 
-}
+// }
+
+//void FillArray(int[] collection);
+
+// {
+//     int length = collection.Length;
+//     int index = 0;
+//     while (index < length)
+//     {
+//         collection[index] = new Random().Next(1, 10);
+//         index++;
+//     }
+// }
+
+// int[] array = new int[10]; //Создай новый массив, в котором будет 10 элементов
+
+
+
