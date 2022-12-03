e173e8c62123cbe082d9726fc45505d1c6366ac0
Alexandr <ragozzzzinalexandr@gmail.com>
2022-12-03 15:48:38 +0500

    added example

---
+++ Example2/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 175, 24, 34, 466, 34, 63, 34, 91 };
+
+int n = array.Length;
+int find = 34;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+
+}
