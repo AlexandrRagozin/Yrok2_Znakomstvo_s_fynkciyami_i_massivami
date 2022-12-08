d03bd8ba8710c27047425d375ad502d5d47851a2
Alexandr <ragozzzzinalexandr@gmail.com>
2022-12-08 22:43:17 +0500

    12

--- Example2/Program.cs
+++ Example2/Program.cs
@@ -15,20 +15,53 @@
 //     index++;
 
 // }
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
 
-//void FillArray(int[] collection);
+}
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+        int index = 0;
+        int position = -1;
 
-// {
-//     int length = collection.Length;
-//     int index = 0;
-//     while (index < length)
-//     {
-//         collection[index] = new Random().Next(1, 10);
-//         index++;
-//     }
-// }
+        while (index < count)
+        {
+            if (collection[index] == find)
+            {
+                position = index;
+                break;
+            }
+            index++;
+        }
+        return position;;
 
-// int[] array = new int[10]; //Создай новый массив, в котором будет 10 элементов
+}
+int[] array = new int[10]; //Создай новый массив, в котором будет 10 элементов
 
+FillArray(array);
+array[4] = 4;
+array[7] = 4;
+PrintArray(array);
 
+Console.WriteLine();
+int pos = IndexOf(array, 444);
+Console.WriteLine(pos);
 
